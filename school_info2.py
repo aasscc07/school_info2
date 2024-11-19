54f43f3c76fe6d2c55ee721dc17fce01462c822e
aasscc07 <75631159+aasscc07@users.noreply.github.com>
2024-11-19 10:22:51 +0900

    Create school_info2.py

---
+++ school_info2.py
@@ -0,0 +1,45 @@
+import time
+
+
+label = ["하수","중수","고수"]
+
+re_time = [0,0,0]
+
+
+def trans_S_time(Stime):
+    
+
+while True:
+    state = int(input(" 시작 : 1 , 종류 : 2"))
+    if state == 1:
+        st = time.time()
+
+        temp = input("공부중...(종류 하려면 아무키나 누르세요)")
+        et = time.time()
+
+
+        dt = et - st
+
+        
+
+        if dt > 6000:
+            print(f"당신은 {label[0]}입니다. ")
+        elif
+
+        
+
+        
+
+        
+
+        
+    elif state == 2:
+        exit()
+    else:
+        "다시 입려해 주세요"
+        continue
+
+    
+
+        
+    
